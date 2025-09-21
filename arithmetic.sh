493ef50714227ea1c093851209b20554cfb3165f
azharmd-dev <azharmd0794@gmail.com>
2025-09-21 13:35:01 +0530

    arithmetic

--- arithmetic.sh
+++ arithmetic.sh
@@ -3,11 +3,11 @@
 #Date=$(date)
 #echo "Date is :: $Date"
 
-start_time=$(date +%T)
+start_time=$(date +%s)
 
 sleep 10
 
-end_time=$(date +%T)
+end_time=$(date +%s)
 
 Total_time=$(($end_time - $start_time))
 echo " start time is :: $start_time"
