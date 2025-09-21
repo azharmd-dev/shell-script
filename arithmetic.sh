131c3b18b08d73a4b6f1260043093876fcec80aa
azharmd-dev <azharmd0794@gmail.com>
2025-09-21 13:38:19 +0530

    arithmetic

--- arithmetic.sh
+++ arithmetic.sh
@@ -3,13 +3,20 @@
 #Date=$(date)
 #echo "Date is :: $Date"
 
-start_time=$(date +%s)
+####1
+#start_time=$(date +%s)
 
-sleep 10
+#sleep 10
 
-end_time=$(date +%s)
+#end_time=$(date +%s)
 
-Total_time=$(($end_time - $start_time))
-echo " start time is :: $start_time"
-echo "end time is :: $end_time"
-echo "Script execution time :: $Total_time  in sec"
+#Total_time=$(($end_time - $start_time))
+#echo " start time is :: $start_time"
+#echo "end time is :: $end_time"
+#echo "Script execution time :: $Total_time  in sec"
+####1
+
+Num1=100
+Num2=564
+Sum=$(($Num1 + $Num2))
+echo "Sum of two number is:: ${Sum}"
