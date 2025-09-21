efb60f77cd7709dae62faebc447af4d87964a252
azharmd-dev <azharmd0794@gmail.com>
2025-09-21 13:00:09 +0530

    practice 1

--- practice1.sh
+++ practice1.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-Per1="Ramesh"
-Per2="Suresh"
+#Per1="Ramesh"
+#Per2="Suresh"
+
+#echo "$Per1 :: Hi How are you $Per2?"
+#echo "$Per2 :: Hello, I'm fine. What about you $Per1"
+#echo "$Per1 :: I'm good" 
+
+Per1=$1
+Per2=$2
 
 echo "$Per1 :: Hi How are you $Per2?"
 echo "$Per2 :: Hello, I'm fine. What about you $Per1"
