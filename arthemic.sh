8d912d088d1b96bb6aee41df4ba72adb663d4a48
azharmd-dev <azharmd0794@gmail.com>
2025-09-21 13:28:01 +0530

    arithmetic

--- arthemic.sh
+++ arthemic.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-Date=$(date)
- echo "Date is :: $Date"
+#Date=$(date)
+#echo "Date is :: $Date"
+
+start_time=$(date +%s)
+
+sleep 10
+
+end_time=$(date +%s)
+
+Total_time=$(($end_time - $start_time))
+
+echo "Script execution time :: $Total_time  in sec"
