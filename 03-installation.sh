eb761612c25e2cd7f760a21e82342a939aff033c
azharmd-dev <azharmd0794@gmail.com>
2025-09-22 22:33:26 +0530

    To install applications

--- 03-installation.sh
+++ 03-installation.sh
@@ -7,7 +7,7 @@ if [ $USERID -ne 0 ]; then
     exit 1
 fi
 
-dnf install nginx -y 
+dnf install ngiinx -y 
 
 if [ $? -ne 0 ]; then
     echo "Installation of nginx is <<failure>> due to it already exist"
