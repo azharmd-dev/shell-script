1dffc04dc4e84ea1755283c0a9e538440db2cdc1
azharmd-dev <azharmd0794@gmail.com>
2025-09-22 23:24:41 +0530

    Checking whether the application is already installed before installation

---
+++ 06-visualInstallation.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+USERID=$(id -u)
+RD="\e[31m"
+GN="\e[32m"
+YW="\e[33m"
+NC="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+    echo "The user required root privilege access to run the script"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1-ne 0 ]; then
+        echo -e "ERROR >> Installation of the application $2 is $RD FAILURE $NC"
+        exit 1
+    else
+        echo -e "Installtion of the application $2 is $GN SUCCESS $NC"  
+}
+dnf list installed nginx #To checl nginx is installed or not
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo -e "The NGINX already installed so $YW SKIPPING $NC the installation"
+fi
+
+dnf list installed python3 #To checl nginx is installed or not
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "python3"
+else
+    echo -e "The PYTHON already installed so $YW SKIPPING $NC the installation"
+fi
+
+dnf list installed mysql #To checl nginx is installed or not
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "MYSQL"
+else
+    echo -e "The MYSQL already installed so $YW SKIPPING $NC the installation"
+fi
