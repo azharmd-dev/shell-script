520258ba32a9163f98491076a9ca64e3fa9d164a
azharmd-dev <azharmd0794@gmail.com>
2025-09-22 21:09:09 +0530

    comparison of two numbers

--- 01-comparision.sh
+++ 01-comparision.sh
@@ -4,6 +4,6 @@ Number=$1
 
 if [ $Number -lt 10 ]; then
     echo "Given number $Number is less than 10"
-Else
-    Echo "Given number $Number is greater than 10"
+else
+    echo "Given number $Number is greater than 10"
 fi
