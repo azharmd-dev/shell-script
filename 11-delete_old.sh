b7c246d2f23a4c776a227c1856798835f176bcc3
azharmd-dev <azharmd0794@gmail.com>
2025-10-02 16:55:14 +0530

    To delete old files

---
+++ 11-delete_old.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+Userid=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+Log_Folder="/var/log/shell-scrip"
+Script_Name=$(echo $0 | cut -d "." -f1)
+Log_File=$Log_Folder/$Script_Name.log
+
+mkdir -p $Log_Folder
+echo "Script execution is started ::: $(date)" | tee -a $LOG_FILE
+
+Source_Dir=/home/ec2-user/app-logs
+
+if [ ! -d $Source_Dir ]; then
+    echo -e "$Source_Dir does not exit"
+    exit 1
+fi
+
+File_to_delete= $(find $Source_Dir -name "*.log" -type f -mtime +14)
+
+while IFS= read -r filepath
+do
+    echo "Deleting the file: $filepath"
+    rm -rf $filepath
+    echo "Deleted the file: $filepath"
+done <<< $FILES_TO_DELETE
