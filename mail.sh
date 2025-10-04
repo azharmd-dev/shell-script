ca50655c80a840a1be1b0c39c38a4617337f5c9f
azharmd-dev <azharmd0794@gmail.com>
2025-10-04 16:04:47 +0530

    Mail Template

--- mail.sh
+++ mail.sh
@@ -1,19 +1,18 @@
-
 #!/bin/bash
-TO_ADDRESS=$1
-SUBJECT=$2
-ALERT_TYPE=$3
-MESSAGE_BODY=$4
-FORMATTED_BODY=$(printf '%s\n' "$MESSAGE_BODY" | sed -e 's/[]\/$*.^[]/\\&/g')
-IP_ADDRESS=$5
-TO_TEAM=$6
+To_Address=$1
+Subject=$2
+Alert_Type=$3
+Message_Body=$4
+Formatted_Body=$(printf '%s\n' "$Message_Body" | sed -e 's/[]\/$*.^[]/\\&/g')
+IP_Address=$5
+To_Team=$6
 
-FINAL_BODY=$(sed -e "s/TO_TEAM/$TO_TEAM/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/IP_ADDRESS/$IP_ADDRESS/g" -e "s/MESSAGE/$FORMATTED_BODY/g" template.html)
+Final_Body=$(sed -e "s/To_Team/$To_Team/g" -e "s/Alert_Type/$Alert_Type/g" -e "s/IP_Address/$IP_Address/g" -e "s/Message/$Formatted_Body/g" template.html)
 
 {
-echo "To: $TO_ADDRESS"
-echo "Subject: $SUBJECT"
+echo "To: $To_Address"
+echo "Subject: $Subject"
 echo "Content-Type: text/html"
 echo ""
-echo "$FINAL_BODY"
-} | msmtp "$TO_ADDRESS"
+echo "$Final_Body"
+} | msmtp "$To_Address"
