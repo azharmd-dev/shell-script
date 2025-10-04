2efb807c67e11839179a16c19781d4a52b4e02c5
azharmd-dev <azharmd0794@gmail.com>
2025-10-04 17:19:18 +0530

    CPU utilization mail configuration

---
+++ cpumail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+To_Address=$1
+Subject=$2
+Alert_Type=$3
+Message_Body=$4
+IP_Address=$5
+To_Team=$6
+
+Final_Body=$(sed -e "s/To_Team/$To_Team/g" -e "s/Alert_Type/$Alert_Type/g" -e "s/IP_Address/$IP_Address/g" -e "s/Message/$Message_Body/g" template.html)
+
+{
+echo "To: $To_Address"
+echo "Subject: $Subject"
+echo "Content-Type: text/html"
+echo ""
+echo "$Final_Body"
+} | msmtp "$To_Address"
