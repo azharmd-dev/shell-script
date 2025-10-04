339de8078a19c7e16734037fa0e81ed24971d22b
azharmd-dev <azharmd0794@gmail.com>
2025-10-04 15:09:31 +0530

    Disk utilization alert

---
+++ 13-diskusage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+Disk_Usage=$(df -hT | grep -v Filesystem)
+Disk_Threshold=2 #Usually in project it is more(75)
+IP_Address=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+Message=""
+
+while IFS= read -r line
+do
+    Usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    Partition=$(echo $line | awk '{print $7}')
+        if [ $Usage -ge $Disk_Threshold ]; then
+            Messsage+="High Disk Usage on $Parttion :: $Usage % <br>" #"+" it will append the previous message
+        fi
+done <<< $Disk_Usage
+
+sh mail.sh "pkpk34366@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$Message" "$IP_Address" "DevOps Team"
+
+
+
