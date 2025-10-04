652b7b68464d169126b837f2f22deff2c8fb4a47
azharmd-dev <azharmd0794@gmail.com>
2025-10-04 16:04:25 +0530

    Disk Utilization

--- 13-diskusage.sh
+++ 13-diskusage.sh
@@ -1,27 +1,21 @@
 #!/bin/bash
 
-DISK_USAGE=$(df -hT | grep -v Filesystem)
-DISK_THRESHOLD=2 # in project we keep it as 75
-IP_ADDRESS=$(curl -s http://169.254.169.254/latest/meta-data/local-ipv4)
-MESSAGE=""
+Disk_Usage=$(df -hT | grep -v Filesystem)
+Disk_Threshold=0 #Usually in project it is more(75)
+IP_Address=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+Message=""
 
 while IFS= read -r line
 do
-    USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
-    PARTITION=$(echo $line | awk '{print $7}')
-    if [ $USAGE -ge $DISK_THRESHOLD ]; then
-        MESSAGE+="High Disk usage on $PARTITION: $USAGE % <br>" # escaping
-    fi
-done <<< $DISK_USAGE
-
-echo -e "Message Body: $MESSAGE"
-
-sh mail.sh "pkpk34366@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$MESSAGE" "$IP_ADDRESS" "DevOps Team"
-
-# TO_ADDRESS=$1
-# SUBJECT=$2
-# ALERT_TYPE=$3
-# MESSAGE_BODY=$4
-# IP_ADDRESS=$5
-
-
+    Usage=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
+    Partition=$(echo $line | awk '{print $7}')
+        if [ $Usage -ge $Disk_Threshold ]; then
+            Message+="High Disk Usage on $Partition :: $Usage % <br>" #"+" it will append the previous message
+        fi
+done <<< $Disk_Usage
+#if [ -n "$Message" ]; then
+    echo "Message Body: $Message"
+    sh mail.sh "pkpk34366@gmail.com" "High Disk Usage Alert" "High Disk Usage" "$Message" "$IP_Address" "DevOps Team"
+#else
+  #  echo "No partitions crossed threshold."
+#fi
